b139db189ab0761a5c44e90c19eabd26a76889d3
harry <harry_tse@163.com>
2018-12-02 18:02:36 +0800

    emitter

--- src/socket/wx.js
+++ src/socket/wx.js
@@ -1,7 +1,51 @@
 import Socket from './base'
+import EventEmitter from '../core/event-emitter'
 
 class WxSocket extends Socket {
-    
+    constructor (opt) {
+        super(opt)
+        this.socketTask = null
+        this._eventEmitter = new EventEmitter()
+    }
+
+    connect () {
+        return new Promise((resolve, reject) => {
+            this.socketTask = wx.connectSocket({
+                url: this.url,
+                success: () => {
+                    this._eventEmitter.once('open', resolve)
+                },
+                fail: (err) => {
+                    reject(err)
+                }
+            })
+            this.socketTask.onOpen = this._onOpen.bind(this)
+            this.socketTask.onMessage = this._onMessage.bind(this)
+            this.socketTask.onClose = this._onClose.bind(this)
+            this.socketTask.onError = this._onError.bind(this)
+        })
+    }
+
+    _onOpen (header) {
+        this._eventEmitter.emit('open', header)
+    }
+
+    _onClose () {
+        this._eventEmitter.emit('close')
+    }
+
+    _onMessage (res) {
+        const data = res.data
+        this.onmessage
+    }
+
+    _onError () {
+
+    }
+
+    send () {}
+
+    onmessage () {}
 }
 
 export default WxSocket
